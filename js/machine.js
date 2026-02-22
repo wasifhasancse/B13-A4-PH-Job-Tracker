875ceb0c5f08dfc5507171b86ee1a096aa801ab3
Wasif Hasan <wasifhasancse@gmail.com>
2026-02-22 09:30:47 +0600

    develop job card status toggle system

--- js/machine.js
+++ js/machine.js
@@ -2,3 +2,20 @@
 function getLengthCount(element) {
   return element.children.length;
 }
+
+function setStatusBadge(status) {
+  document.getElementById("job-status").innerText = status;
+  document.getElementById("job-status").classList.remove("text-gray-800");
+  
+  if (status === "Interview") {
+    document.getElementById("job-status")
+      .classList.remove("text-red-400", "border-red-300", "bg-red-100");
+    document.getElementById("job-status")
+      .classList.add("text-green-400", "border-green-300", "bg-green-100");
+  } else if (status === "Rejected") {
+    document.getElementById("job-status")
+      .classList.remove("text-green-400", "border-green-300", "bg-green-100");
+    document.getElementById("job-status")
+      .classList.add("text-red-400", "border-red-300", "bg-red-100");
+  }
+}
--- js/script.js
+++ js/script.js
@@ -4,12 +4,16 @@ const interviewCount = document.getElementById("interview-count");
 const rejectedCount = document.getElementById("rejected-count");
 const availableJobCount = document.getElementById("available-job-count");
 
+// dashboard button select
+const allJobBtn = document.getElementById("all-job-button");
+const interviewJobBtn = document.getElementById("interview-job-button");
+const rejectedJobBtn = document.getElementById("rejected-job-button");
+
 const allJobShow = document.getElementById("all-job-show");
 const interviewJobShow = document.getElementById("interview-job-show");
 const interviewJobShowArray = [];
 const rejectedJobShow = document.getElementById("rejected-job-show");
 const rejectedJobShowArray = [];
-
 
 // dashboard total job count
 totalCount.innerText = getLengthCount(allJobShow);
@@ -22,3 +26,43 @@ rejectedCount.innerText = getLengthCount(rejectedJobShow);
 
 // dashboard available job count
 availableJobCount.innerText = getLengthCount(allJobShow);
+
+// job showing button toggle event
+document.getElementById("job-show-buttons").addEventListener("click", function (event) {
+    const clickedButton = event.target;
+
+    allJobBtn.classList.add(
+      "text-gray-600",
+      "border-2",
+      "border-gray-200",
+      "bg-gray-50",
+    );
+    interviewJobBtn.classList.add(
+      "text-gray-600",
+      "border-2",
+      "border-gray-200",
+      "bg-gray-50",
+    );
+    rejectedJobBtn.classList.add(
+      "text-gray-600",
+      "border-2",
+      "border-gray-200",
+      "bg-gray-50",
+    );
+
+    clickedButton.classList.remove(
+      "text-gray-600",
+      "border-2",
+      "border-gray-200",
+      "bg-gray-50",
+    );
+    clickedButton.classList.add(
+      "text-white",
+      "border-2",
+      "border-blue-500",
+      "bg-blue-500",
+    );
+    console.log("clicked", event.target);
+    console.log("clicked", event.target.id);
+  });
+
